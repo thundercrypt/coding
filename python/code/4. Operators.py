5300c9410a524a3e5be50a61099ca13dfccd9607
thundercrypt <77282722+thundercrypt@users.noreply.github.com>
2021-01-12 15:04:02 +0530

    Update 4. Operators.py

--- python/code/4. Operators.py
+++ python/code/4. Operators.py
@@ -1,2 +1,13 @@
 # Arithmetic Operators
 # Let's take any two variables and perform arithmetic operations on them.
+x=3
+y=4
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x%y)
+print(x//y)
+
+# Let's take the same two variables and use comparision operators on them.
