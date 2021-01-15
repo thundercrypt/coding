48c7a817d00b10219fd39d8ff22604c879bc390d
thundercrypt <77282722+thundercrypt@users.noreply.github.com>
2021-01-15 09:39:39 +0530

    Create 16. Table of a Number.py

---
+++ python/code/16. Table of a Number.py
@@ -0,0 +1,4 @@
+# This program calculates the table of a number up until the tenth table, using the 'for' loop.
+num = int(input("Please enter a number to print it's table: "))
+for x in range(1,11):
+    print(num,"*",i,"=",num*i)
