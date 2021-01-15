fd1b108f9afddcabbd4d98d13e5bb42904bfcf17
thundercrypt <77282722+thundercrypt@users.noreply.github.com>
2021-01-15 11:18:30 +0530

    Create 17

---
+++ python/code/17. Print the sum of first 'n' natural numbers.py
@@ -0,0 +1,10 @@
+# This program prints the sum of the first number entered by the user.
+userNum = int(input("Please enter a natural number: "))
+sum=0
+for i in range(userNum, userNum+1):
+    sum=sum+1
+
+print(sum)
+
+
+# WORK ON THIS
