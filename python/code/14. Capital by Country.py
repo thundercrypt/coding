7c8f34072ea17c5232c0c1ab8838a90fad02eb60
thundercrypt <77282722+thundercrypt@users.noreply.github.com>
2021-01-22 14:59:44 +0530

    Update 14. Capital by Country.py

--- python/code/14. Capital by Country.py
+++ python/code/14. Capital by Country.py
@@ -1,8 +1,8 @@
 # This program gives the capital of the country entered by the user.
 userCountry = input("Please enter a Country Name: ")
 
-if("India" in userCountry): print("The capital of",userCountry,"is New Delhi")
-elif("Australia" in userCountry): print("The capital of",userCountry,"is Canberra")
-elif("France" in userCountry): print("The capital of",userCountry,"is Paris")
-elif("Japan" in userCountry): print("The capital of",userCountry,"is Tokyo")
+if("India" in userCountry): print("The capital of",userCountry,"is New Delhi.")
+elif("Australia" in userCountry): print("The capital of",userCountry,"is Canberra.")
+elif("France" in userCountry): print("The capital of",userCountry,"is Paris.")
+elif("Japan" in userCountry): print("The capital of",userCountry,"is Tokyo.")
 else: print("Please enter a country included in the database.")
