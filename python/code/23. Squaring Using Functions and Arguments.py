e95b35194069c8e42033d1e9ad134579991888c6
thundercrypt <77282722+thundercrypt@users.noreply.github.com>
2021-01-28 10:35:35 +0530

    Update 23.

--- python/code/23. Squaring Using Functions and Arguments.py
+++ python/code/23. Squaring Using Functions and Arguments.py
@@ -1,6 +1,7 @@
 # Define the function
 def square(num):
-    return num**2
+    return(num**2)
+
 
 # Ask the user for input
 userNum = int(input("Please enter an integer to be squared: "))
@@ -8,4 +9,4 @@ userNum = int(input("Please enter an integer to be squared: "))
 
 # Call the function
 
-print("The square of",userNum,"is",square(userNum),".")
+print("The square of", userNum, "is", square(userNum), ".")
