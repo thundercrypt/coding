f8bfb2426cf1a0b0e90db80506a82c6247f5dba3
thundercrypt <77282722+thundercrypt@users.noreply.github.com>
2021-03-02 20:47:52 +0530

    Update @.

---
+++ Python/2. Strings.py
@@ -0,0 +1,5 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + "world")
