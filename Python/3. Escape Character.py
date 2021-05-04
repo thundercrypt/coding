1916b2b6f1476602871c221769e5443ab62f6e26
thundercrypt <77282722+thundercrypt@users.noreply.github.com>
2021-05-04 20:44:40 +0530

    Update 3.

--- Python/3. Escape Character.py
+++ Python/3. Escape Character.py
@@ -4,4 +4,15 @@ print(splitString)
 tabbedString = "1\t2\t3\t4\t5"
 print(tabbedString)
 
-print('TThe pet shop owner said')
+print('The pet shop owner said "No, no, \'e\'s uh, ... he\'s resting".')
+# or
+print("The pet shop owner said \"No, no, 'e's uh, ... he's resting\".")
+
+print("""The pet shop owner said "No, no, 'e's uh, ... he's resting".""")
+
+anotherSplitString = """This string has been \
+split over \
+several \
+lines"""
+
+print(anotherSplitString)
